fc571a51b4fc8615d9b471c21506fe0c1f74417e
LundiLanga <alulundilanga@gmail.com>
2025-02-13 14:16:11 +0200

    Updated the names of the testing methods

--- src/test/java/com/calculator/calculatorTest.java
+++ src/test/java/com/calculator/calculatorTest.java
@@ -15,14 +15,14 @@ public class calculatorTest
      //Adding numbers
 
     @Test
-    public void testAddTwoNumbers()
+    public void AddTwoNumbersTest()
     {
         assertEquals(3, calculator.addTwoNumbers(1, 2)); 
         assertEquals(-2, calculator.addTwoNumbers(-1, -1));
     }
     
     @Test
-    public void testAddMultipleNumbers(){
+    public void AddMultipleNumbersTest(){
         assertEquals(15, calculator.addMultipleNumbers(1, 2, 3, 4, 5)); 
         assertEquals(3, calculator.addMultipleNumbers(1, 2)); 
         assertEquals(-2, calculator.addMultipleNumbers(-1, -1));
@@ -31,14 +31,14 @@ public class calculatorTest
     //Multiplying numbers
 
     @Test
-    public void testMultiplyTwoNumbers()
+    public void MultiplyTwoNumbersTest()
     {
         assertEquals(3, calculator.MultiplyTwoNumbers(1, 3)); 
         assertEquals(-3, calculator.MultiplyTwoNumbers(-1, 3));
     }
 
     @Test
-    public void testMultiplyMultipleNumbers(){
+    public void MultiplyMultipleNumbersTest(){
         assertEquals(120, calculator.MultiplyMultipleNumbers(1, 2, 3, 4, 5)); 
         assertEquals(3, calculator.MultiplyMultipleNumbers(1, 3)); 
         assertEquals(3, calculator.MultiplyMultipleNumbers(-1, -3));
